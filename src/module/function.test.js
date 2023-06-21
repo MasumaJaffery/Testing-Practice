f7603f4c297f79787295a688fff820d39e001593
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-06-21 03:25:28 -0700

    Fix-Linters

--- src/module/function.test.js
+++ src/module/function.test.js
@@ -1,6 +1,7 @@
 const { stringlength } = require('./function.js').default;
 test('check if the string is at least 1 character long and not longer than 10 characters', () => {
   if(!stringlength || stringlength.length < 1 || stringlength.length > 10 ) {
+    //!stringlength checks length is 0 or str is empty!
     throw 'error';
   }
 });
